1c11971099cd774720da0f897cc132845b79411e
Lucky <lucky3028@users.noreply.github.com>
2021-09-07 23:30:28 +0900

    [Remove] TODO

--- src/domains/github_api_result.rs
+++ src/domains/github_api_result.rs
@@ -15,7 +15,6 @@ pub struct ContainerImage {
     pub id: usize,
     pub name: String,
     pub metadata: ContainerMetadata,
-    // TODO: タイムゾーンがUTCなので日付時刻にパースした上で直す
     pub created_at: String,
     pub updated_at: String,
 }
